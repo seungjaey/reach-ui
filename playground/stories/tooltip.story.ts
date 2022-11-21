c27f9bf54c9a63d3bc01443fd63bc292ab5ba15e
seungjaey <seungjaey@gmail.com>
2022-11-21 13:35:24 +0900

    v1

--- playground/stories/tooltip.story.ts
+++
@@ -1,7 +0,0 @@
-export { Example as Animated } from "./tooltip/animated.example";
-export { Example as Basic } from "./tooltip/basic.example";
-export { Example as CustomTooltip } from "./tooltip/custom-tooltip.example";
-export { Example as Triangle } from "./tooltip/triangle.example";
-export { Example as WithDialog } from "./tooltip/with-dialog.example";
-
-export default { title: "Tooltip" };
--- playground/stories/visually-hidden.story.ts
+++
@@ -1,3 +0,0 @@
-export { Example as Basic } from "./visually-hidden/basic.example";
-
-export default { title: "VisuallyHidden" };
